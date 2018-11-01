1910fc11bfc44e4356379a8ead4f5a29e20ad0e0
Andrew Huth <andrew@huth.me>
2018-10-31 21:14:39 -0700

    Add a canvas

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
-export default function App() {
-  return <span>hello world!</span>;
+export default class App extends React.Component {
+  canvasRef = React.createRef();
+
+  componentDidMount() {
+    this.ctx = this.canvasRef.current.getContext('2d');
+  }
+
+  render() {
+    return (
+      <canvas
+        height={240}
+        ref={this.canvasRef}
+        style={styles.canvas}
+        width={320}
+      />
+    );
+  }
 }
+
+const styles = {
+  canvas: {
+    height: 480,
+    width: 640,
+  },
+};
