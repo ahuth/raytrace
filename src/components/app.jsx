6a828d8f7815c3198fdad08a69ac9a853530f0cd
Andrew Huth <andrew@huth.me>
2018-11-03 16:06:19 -0700

    Add controls for starting and stopping

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,19 +2,34 @@ import React from 'react';
 
 export default class App extends React.Component {
   canvasRef = React.createRef();
+  state = { playing: false };
 
   componentDidMount() {
     this.ctx = this.canvasRef.current.getContext('2d');
   }
 
+  handlePlay = () => {
+    this.setState({ playing: true });
+  }
+
+  handleStop = () => {
+    this.setState({ playing: false });
+  }
+
   render() {
     return (
-      <canvas
-        height={240}
-        ref={this.canvasRef}
-        style={styles.canvas}
-        width={320}
-      />
+      <div style={styles.container}>
+        <canvas
+          height={240}
+          ref={this.canvasRef}
+          style={styles.canvas}
+          width={320}
+        />
+        <div>
+          <button onClick={this.handlePlay}>Play</button>
+          <button onClick={this.handleStop}>Stop</button>
+        </div>
+      </div>
     );
   }
 }
@@ -23,5 +38,10 @@ const styles = {
   canvas: {
     height: 480,
     width: 640,
+  },
+  container: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
   },
 };
