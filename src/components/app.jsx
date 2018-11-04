3afb7154d6590abf7b81569440dbafdd45d5ca0d
Andrew Huth <andrew@huth.me>
2018-11-03 17:28:20 -0700

    Display the rendered output!!!

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,48 @@
 import React from 'react';
+import * as scene from '../scene';
+import render from '../utils/render';
 
 export default class App extends React.Component {
   canvasRef = React.createRef();
   playing = false;
+  planet1 = 0;
+  planet2 = 0;
 
   componentDidMount() {
     this.ctx = this.canvasRef.current.getContext('2d');
+    this.pixelData = this.ctx.getImageData(0, 0, 320, 240);
+    this.tick();
   }
 
   handlePlay = () => {
     this.playing = true;
+    this.tick();
   }
 
   handleStop = () => {
     this.playing = false;
+  }
+
+  tick = () => {
+    // Straight-line distance covered. Note that planet2 orbits a bit faster.
+    this.planet1 += 0.1;
+    this.planet2 += 0.2;
+
+    // Convet straight-line distance to orbital positions with some trigonometry.
+    scene.objects[1].point.x = Math.sin(this.planet1) * 3.5;
+    scene.objects[1].point.z = -3 + (Math.cos(this.planet1) * 3.5);
+
+    scene.objects[2].point.x = Math.sin(this.planet2) * 4;
+    scene.objects[2].point.z = -3 + (Math.cos(this.planet2) * 4);
+
+    // Render everything.
+    const nextPixelData = render(scene, 240, 320, this.pixelData);
+    this.ctx.putImageData(nextPixelData, 0, 0);
+
+    // Move the planets and render again.
+    if (this.playing) {
+      setTimeout(this.tick, 10);
+    }
   }
 
   render() {
